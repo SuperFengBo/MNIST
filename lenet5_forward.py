0b09065796c7433a0ccbfed08698ef149f7cc9a7
Youth <541614850@qq.com>
2020-10-14 17:20:00 +0800

    acc=99.38 num+100% pic100% mnist_test_jpg_10000 100%

--- lenet5_forward.py
+++ lenet5_forward.py
@@ -3,14 +3,15 @@ IMAGE_CHANNEL=1
 OUTPUT_NODE=10
 import tensorflow as tf
 import numpy as np
-CONV1_SIZE=3
+CONV1_SIZE=5
 CONV1_CHANNEL=1
-CONV1_KERNEL_NUM=2
+CONV1_KERNEL_NUM=32
 CONV2_SIZE=5
 CONV2_CHANNEL=CONV1_KERNEL_NUM
-CONV2_KERNEL_NUM=2
+CONV2_KERNEL_NUM=64
 regularizer=0.0001
-def get_weight(shape,regularizer):
+FC1_SIZE=512
+def get_weight(shape,regulaizer):
     w = tf.Variable(tf.truncated_normal(shape,stddev=0.1))
     if regularizer!=None:
         tf.contrib.layers.l2_regularizer(regularizer)(w)
@@ -18,18 +19,30 @@ def get_weight(shape,regularizer):
 def get_bias(shape):
     b = tf.Variable(tf.zeros(shape))
     return b
-def forward(x,regularizer):
-    conv1_w=get_weight([CONV1_SIZE,CONV1_SIZE,CONV1_CHANNEL,CONV1_KERNEL_NUM],regularizer)
+def forward(x,train,regularizer):
+    conv1_w=get_weight([CONV1_SIZE,CONV1_SIZE,IMAGE_CHANNEL,CONV1_KERNEL_NUM],regularizer)
     conv1_b=get_bias([CONV1_KERNEL_NUM])
-    y1=tf.nn.conv2d(x,conv1_w,[1,1,1,1],padding='SAME')+conv1_b
-    print('***************y1',y1)
-    conv2_w=get_weight([CONV2_SIZE,CONV2_SIZE,CONV2_CHANNEL,CONV2_KERNEL_NUM],regularizer)
+    conv1=tf.nn.conv2d(x,conv1_w,[1,1,1,1],padding='SAME')
+    relu1=tf.nn.relu(tf.nn.bias_add(conv1,conv1_b))
+    pool1=tf.nn.max_pool(relu1,[1,2,2,1],[1,2,2,1],padding='SAME')
+
+    conv2_w=get_weight([CONV2_SIZE,CONV2_SIZE,CONV1_KERNEL_NUM,CONV2_KERNEL_NUM],regularizer)
     conv2_b=get_bias([CONV2_KERNEL_NUM])
-    y2=tf.nn.conv2d(y1,conv2_w,[1,1,1,1],padding='SAME')+conv2_b    
+    conv2=tf.nn.conv2d(pool1,conv2_w,[1,1,1,1],padding='SAME')  
+    relu2=tf.nn.relu(tf.nn.bias_add(conv2,conv2_b))
+    pool2=tf.nn.max_pool(relu2,[1,2,2,1],[1,2,2,1],padding='SAME')
+    pool_shape=pool2.get_shape().as_list()
+
+    y_reshape=tf.reshape(pool2,[-1,pool_shape[1]*pool_shape[2]*pool_shape[3]])    
     
-    y=tf.reshape(y2,[-1,IMAGE_SIZE*IMAGE_SIZE*CONV2_KERNEL_NUM])    
-    
-    fc1_w=get_weight([IMAGE_SIZE*IMAGE_SIZE*CONV2_KERNEL_NUM,OUTPUT_NODE],regularizer)
-    fc1_b=get_bias([OUTPUT_NODE])
-    y_fc=tf.matmul(y,fc1_w)+fc1_b
-    return y_fc
+    fc1_w=get_weight([pool_shape[1]*pool_shape[2]*pool_shape[3],FC1_SIZE],regularizer)
+    fc1_b=get_bias(FC1_SIZE)
+    fc1=tf.matmul(y_reshape,fc1_w)+fc1_b
+    if train: 
+        fc1 = tf.nn.dropout(fc1, 0.5)
+
+    fc2_w=get_weight([FC1_SIZE,OUTPUT_NODE],regularizer)
+    fc2_b=get_bias([OUTPUT_NODE])
+    fc2=tf.matmul(fc1,fc2_w)+fc2_b   
+
+    return fc2
--- lenet5_test.py
+++ lenet5_test.py
@@ -6,12 +6,13 @@ import numpy as np
 import lenet5_forward
 BATCH_SIZE=5000
 mnist=input_data.read_data_sets("./data/",one_hot=True)
+train=False
 def test(mnist):
     with tf.Graph().as_default() as g:
 
         x=tf.placeholder(tf.float32,[None,lenet5_forward.IMAGE_SIZE,lenet5_forward.IMAGE_SIZE,lenet5_forward.IMAGE_CHANNEL])
         y_=tf.placeholder(tf.float32,[None,mnist_forward.OUTPUT_NODE])
-        y=lenet5_forward.forward(x,lenet5_forward.regularizer)
+        y=lenet5_forward.forward(x,train,lenet5_forward.regularizer)
 
         correct_pre=tf.equal(tf.arg_max(y,1),tf.arg_max(y_,1)) 
         correct_pre=tf.cast(correct_pre,tf.float32)
